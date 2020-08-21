21073c69b966ad352ec6dbd221d56b0be730e357
Fzuerzmj <794858610@qq.com>
2020-08-22 00:58:11 +0800

    管理员界面，读者管理修改和删除读者
    管理员界面，读者管理修改和删除读者

--- src/Admin/ReaderManagePanel.java
+++ src/Admin/ReaderManagePanel.java
@@ -4,7 +4,10 @@
 
 package Admin;
 
+import Beans.Reader;
 import Beans.Record;
+import Utils.DBConnect;
+import sun.security.pkcs11.Secmod;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -36,6 +39,7 @@ public class ReaderManagePanel extends JPanel {
         button3 = new JButton();
         scrollPane1 = new JScrollPane();
         panel1 = new JPanel();
+        textArea1.setText("\u7528\u6237\u540d");
 
         //======== this ========
         setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder( 0
@@ -59,12 +63,34 @@ public class ReaderManagePanel extends JPanel {
         add(button1);
         button1.setBounds(740, 20, 25, 25);
 
+/*搜索按钮监听器*/
+        //给搜索按钮添加事务监听器
+        button1.addActionListener(e->{
+            String getReaderID = textField1.getText().toString();  //待检索的用户ID
+            readerID = Integer.parseInt(getReaderID);
+            DBConnect dbConnect = new DBConnect();
+            Reader readerMes = dbConnect.queryReaderInformation(readerID);
+            if(readerMes.getReaderId() == -1){  //待检索的用户不存在
+                textArea1.setText("\u7528\u6237\u540d");
+                textField2.setText("");  //地址
+                textField3.setText("");  //号码
+                textField4.setText(""); //借书限制
+                JOptionPane.showMessageDialog(null,"用户不存在，请从新检索！","Error",JOptionPane.ERROR_MESSAGE);
+            }
+            else{     //将检索的用户数据填入其中
+                textField2.setText(readerMes.getAddress());  //地址
+                textField3.setText(readerMes.getPhoneNumber());  //号码
+                textField4.setText(String.valueOf(readerMes.getLimits())); //借书限制
+                textArea1.setText(readerMes.getName());
+            }
+        });
+
+
         //---- textArea1 ----
         textArea1.setFocusable(false);
         textArea1.setOpaque(false);
         textArea1.setEditable(false);
         textArea1.setFont(new Font("\u5fae\u8f6f\u96c5\u9ed1", Font.PLAIN, 26));
-        textArea1.setText("\u7528\u6237\u540d");
         add(textArea1);
         textArea1.setBounds(90, 75, 100, textArea1.getPreferredSize().height);
 
@@ -89,7 +115,7 @@ public class ReaderManagePanel extends JPanel {
         //---- textField2 ----
         textField2.setOpaque(false);
         textField2.setBackground(Color.white);
-        textField2.setEditable(false);
+        textField2.setEditable(true);
         textField2.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 16));
         add(textField2);
         textField2.setBounds(305, 80, 430, 25);
@@ -97,7 +123,7 @@ public class ReaderManagePanel extends JPanel {
         //---- textField3 ----
         textField3.setOpaque(false);
         textField3.setBackground(Color.white);
-        textField3.setEditable(false);
+        textField3.setEditable(true);
         textField3.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 16));
         add(textField3);
         textField3.setBounds(305, 120, 430, 25);
@@ -105,7 +131,7 @@ public class ReaderManagePanel extends JPanel {
         //---- textArea4 ----
         textArea4.setFocusable(false);
         textArea4.setOpaque(false);
-        textArea4.setEditable(false);
+        textArea4.setEditable(true);
         textArea4.setFont(new Font("\u5fae\u8f6f\u96c5\u9ed1", Font.PLAIN, 16));
         textArea4.setText("\u501f\u9605\u9650\u989d\uff1a");
         add(textArea4);
@@ -120,10 +146,32 @@ public class ReaderManagePanel extends JPanel {
         add(button2);
         button2.setBounds(115, 420, 305, 30);
 
+        //修改读者信息按钮监听器
+        button2.addActionListener(e->{
+            String address = textField2.getText();
+            String phone = textField3.getText();
+            String limit = textField4.getText();
+
+            if(address.equals("") || phone.equals("") || limit.equals("")){  //修改信息为空
+                JOptionPane.showMessageDialog(null,"信息不能为空！","Error",JOptionPane.ERROR_MESSAGE);
+            }
+            else{  //修改
+                Reader reader = new Reader(readerID,textArea1.getText(),"",address,phone,Integer.parseInt(limit));
+                DBConnect dbConnect = new DBConnect();
+                int status = dbConnect.modifyReaderInformation(reader);
+                if(status == 0)   //修改成功
+                    JOptionPane.showMessageDialog(null,"修改读者信息成功！","Success",JOptionPane.INFORMATION_MESSAGE);
+                else
+                    JOptionPane.showMessageDialog(null,"修改读者信息失败，请稍后重试！","Error",JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+
+
         //---- textField4 ----
         textField4.setOpaque(false);
         textField4.setBackground(Color.white);
-        textField4.setEditable(false);
+        textField4.setEditable(true);
         textField4.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 16));
         add(textField4);
         textField4.setBounds(305, 155, 95, 25);
@@ -136,6 +184,33 @@ public class ReaderManagePanel extends JPanel {
         button3.setBorder(null);
         add(button3);
         button3.setBounds(525, 420, 305, 30);
+
+        //删除读者按钮监听器
+        button3.addActionListener(e->{
+            if(readerID != 0){
+                int result = JOptionPane.showConfirmDialog(null,"确定删除读者：  " + readerID + "  吗？", "删除用户确认",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);   //确认删除
+                if(result == JOptionPane.YES_OPTION){
+                    DBConnect dbConnect = new DBConnect();
+                    int status = dbConnect.deleteReader(readerID);
+                    if(status == 0) {  //成功删除
+                        JOptionPane.showMessageDialog(null,"已成功删除读者：" + readerID + "。","Success",JOptionPane.INFORMATION_MESSAGE);
+                        textArea1.setText("\u7528\u6237\u540d");
+                        textField2.setText("");  //地址
+                        textField3.setText("");  //号码
+                        textField4.setText(""); //借书限制
+                    }
+                    else
+                        JOptionPane.showMessageDialog(null,"删除读者失败，请稍后重试！","Error",JOptionPane.ERROR_MESSAGE);
+                }
+
+
+
+            }else {
+                JOptionPane.showMessageDialog(null,"请先检索需要删除的用户！","Error",JOptionPane.ERROR_MESSAGE);
+            }
+        });
 
         //======== scrollPane1 ========
         {
@@ -220,5 +295,6 @@ public class ReaderManagePanel extends JPanel {
     private JButton button3;
     private JScrollPane scrollPane1;
     private JPanel panel1;
+    int readerID = 0;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
